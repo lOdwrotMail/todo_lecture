a5c5b30971b77afbacdc93c4e27815dc0b1954c3
lOdwrotMail <lukasz.o@mate.academy>
2023-09-11 10:59:06 +0200

    lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,153 @@
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import cn from 'classnames';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+interface Color {
+  id: number;
+  name: string;
+}
+
+interface GoodWithoutColor {
+  id: number;
+  name: string;
+  colorId: number;
+}
+
+const colors: Color[] = [
+  { id: 1, name: 'red' },
+  { id: 2, name: 'green' },
+  { id: 3, name: 'blue' },
+];
+
+interface Good extends GoodWithoutColor {
+  color: Color | null;
+}
+
+const goodsFromServer: GoodWithoutColor[] = [
+  { id: 1, colorId: 1, name: 'Dumplings' },
+  { id: 2, colorId: 2, name: 'Carrot' },
+  { id: 3, colorId: 3, name: 'Eggs' },
+  { id: 4, colorId: 1, name: 'Ice cream' },
+  { id: 5, colorId: 2, name: 'Apple' },
+  { id: 6, colorId: 3, name: 'Bread' },
+  { id: 7, colorId: 1, name: 'Fish' },
+  { id: 8, colorId: 2, name: 'Honey' },
+  { id: 9, colorId: 3, name: 'Jam' },
+  { id: 10, colorId: 1, name: 'Garlic' },
+];
+
+const concatedGoods: Good[] = goodsFromServer.map((good) => ({
+  ...good,
+  color: colors.find((color) => color.id === good.colorId) || null,
+}));
 
 export const App = () => {
+  const [goods, setGoods] = useState<Good[]>(concatedGoods);
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
-
-      <form action="/api/todos" method="POST">
-        <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
-        </div>
-
-        <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
-
-          <span className="error">Please choose a user</span>
-        </div>
-
-        <button type="submit" data-cy="submitButton">
-          Add
-        </button>
-      </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <GoodForm
+        colors={colors}
+        onAddGood={(good) => {
+          setGoods((prevState) => [...prevState, good]);
+        }}
+      />
+      <GoodsList goods={goods} />
     </div>
+  );
+};
+
+type GoodsListProps = {
+  goods: Good[];
+};
+
+export const GoodsList = ({ goods }: GoodsListProps) => {
+  return (
+    <ul>
+      {goods.map((good) => (
+        <li key={good.id} style={{ color: good.color?.name }}>
+          {good.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+type GoodFormProps = {
+  colors: Color[];
+  onAddGood: (good: Good) => void;
+};
+
+export const GoodForm = ({ colors, onAddGood }: GoodFormProps) => {
+  const [name, setName] = useState<string>('');
+  const [color, setColor] = useState<Color | null>(null);
+  const [hasColorError, setHasColorError] = useState(false);
+  const [hasNameError, setHasNameError] = useState(false);
+
+  const handleChangeName: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setName(e.target.value);
+    setHasNameError(false);
+  };
+
+  const handleColorChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setColor(colors.find((v) => v.id === Number(e.target.value)) || null);
+    setHasColorError(false);
+  };
+
+  const handleSubmit: FormEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setHasNameError(true);
+    }
+
+    if (!color) {
+      setHasColorError(true);
+    }
+
+    if (!name || !color) {
+      return;
+    }
+
+    const good: Good = {
+      id: new Date().getTime(),
+      colorId: color.id,
+      name,
+      color,
+    };
+
+    onAddGood(good);
+
+    setName('');
+    setColor(null);
+  };
+
+  return (
+    <form>
+      <input
+        value={name}
+        onChange={handleChangeName}
+        className={cn({ 'with-error': hasNameError })}
+      />
+      <select
+        value={color?.id || 0}
+        defaultValue={0}
+        onChange={handleColorChange}
+        className={cn({ 'with-error': hasColorError })}
+      >
+        <option value={0} disabled>
+          Select Value
+        </option>
+        {colors.map((color) => (
+          <option key={color.id} value={color.id}>
+            {color.name}
+          </option>
+        ))}
+      </select>
+      <button type="submit" onClick={handleSubmit}>
+        Add
+      </button>
+    </form>
   );
 };
